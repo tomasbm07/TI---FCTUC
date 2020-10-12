3ddddd052c472341135892318db54df08c3aef9f
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-10-12 11:37:20 +0100

    Add new files

--- TP1/main.py
+++ TP1/main.py
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 import Histograma as hst
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,5 +7,4 @@ PATH = "//data"
 teste = [1,2,2,2,64,78,78,2,3,4,5,5,5,5,5,5,5]
 
 hst.show_histograma(hst.cria_valores(teste), hst.cria_hist(teste))
-	
->>>>>>> Stashed changes
+
