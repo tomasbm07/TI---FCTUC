458e5f109c191abb65ca750ef3bcbd62e9576fe4
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-10-12 14:21:09 +0100

    Add files

--- TP1/main.py
+++ TP1/main.py
@@ -1,11 +1,10 @@
-import idk 
+import Histograma as hst
 import numpy as np
 import matplotlib.pyplot as plt
 
 PATH = "//data"
 
-data = [1,2,3,2,5,64]
-values = [1,2,3,3,5,50]
+teste = [1,2,2,2,64,78,78,2,3,4,5,5,5,5,5,5,5]
 
-idk.show_histograma(data,values)
-	
+hst.show_histograma(hst.cria_valores(teste), hst.cria_hist(teste))
+
