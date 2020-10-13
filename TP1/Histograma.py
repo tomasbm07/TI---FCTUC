50648f467f2ae841a626057a25f5e5e932712502
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-10-13 15:52:01 +0100

    Add represent any file feature

--- TP1/Histograma.py
+++ TP1/Histograma.py
@@ -1,5 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as img
+import sounddevice as sd
+from scipy.io import wavfile
+
+PATH = "data\\"
+
+
+#Represents a numpy array in a histogram
 def show_histograma(data):
     x, values = np.unique(data, return_counts=True)
     plt.figure(0)
@@ -8,3 +16,26 @@ def show_histograma(data):
     plt.ylabel("Repetições")
     plt.bar(x, values, align = "center")
     plt.show()
+
+#Selects how to represent each file type
+def histograma(file):
+	#Text
+	if(".txt" in file):
+		file = open(PATH + file, "r")
+		text = np.asarray(list(file.read()))
+		show_histograma(text)
+
+	#Images
+	if(".bmp" in file):
+		image = img.imread(PATH + file)
+		#Check se a imagem é RGBA ou Grayscale
+		if(image.ndim == 2):
+			show_histograma(image) #Grayscale
+		else:
+			show_histograma(image[:,:,:3]) #RGBA
+
+	#Sound
+	if(".wav" in file):
+		sr, sound = wavfile.read(PATH + "saxriff.wav") #returns Sample Rate and Data
+		sound = np.asarray(sound)
+		show_histograma(sound)
--- TP1/main.py
+++ TP1/main.py
@@ -6,23 +6,13 @@ import sounddevice as sd
 from scipy.io import wavfile
 
 
-PATH = "data\\"
+def main():
+	# lena.bmp, CT1.bmp, binaria.bmp, saxriff.wav, texto.txt
+	hst.histograma("lena.bmp")
 
 
-"""img = img.imread(PATH + "lena.bmp")
-img = np.asarray(img)
-hst.show_histograma(img)"""
-
-img = img.imread(PATH + "CT1.bmp")
-img = np.asarray(img)
-#print(img[:,:,:3])
-hst.show_histograma(img[:,:,:3])
-
-"""img = img.imread(PATH + "binaria.bmp")
-img = np.asarray(img)
-hst.show_histograma(img)"""
-
-#,sound = wavfile.read(PATH + "saxriff.wav")
-#sound = np.asarray(sound)
 
 
+
+
+main()
