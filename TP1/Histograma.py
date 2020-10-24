34e9a8981658ca733b540f864c13c14a8e5b1f4d
joelpoliveira <58565967+joelpoliveira@users.noreply.github.com>
2020-10-24 20:19:11 +0100

    commit joeu
    agrupamento de simbolos , em principio, funcional

--- TP1/Histograma.py
+++ TP1/Histograma.py
@@ -7,7 +7,14 @@ from scipy.stats import entropy
 import math
 PATH = "data\\"
 
-#calc entropia com math
+#calc entropia com np.log2
+def np_entropia(valores):
+    h=0
+    total = np.sum(valores)
+    for i in valores:
+        h+=-np.log2(i/total)*(i/total)
+    return h
+
 #contar apenas letras do alfabeto regulares e digitos
 def uniques(data):
     symbols = []
@@ -35,13 +42,13 @@ def show_histograma(data,flag):
     plt.bar(x, values, align="center")
     plt.show()
 
-def show_histograma_groups(data):
-    x, values = alfabeto(data)
+def show_histograma_groups(data,istext,isbmp):
+    x, values = group_symbols(data,istext,isbmp)
     print(entropy(values,base=2))
-    plt.figure(0)
+    plt.figure(1)
     plt.annotate(f'H = {entropy((values),qk=None,base=2):.2f} bits/pixel', xy=(0, 0), xycoords=('axes fraction', 'figure fraction'),
                  xytext=(65, 5), textcoords='offset points', size=12, ha='right', va='bottom')
-    plt.title("Histograma")
+    plt.title("Histograma Groups")
     plt.xlabel("Valores")
     plt.ylabel("Repetições")
     plt.bar(x, values, align="center")
@@ -53,52 +60,87 @@ def histograma(file):
         file = open(file, "r")
         text = np.asarray(list(file.read()))
         #show_histograma(text,True)
-        show_histograma_groups(text)
+        show_histograma_groups(text,True,False)
     # Images
     if ".bmp" in file:
         image = img.imread(file)
         # Check se a imagem é RGBA ou Grayscale
         if image.ndim == 2:
-            show_histograma(image,file)  # Grayscale
+            #show_histograma(image,file)  # Grayscale
+            show_histograma_groups(image,False,True)
         else:
-            show_histograma(image[:, :, :1],False)  # RGBA, apenas mostra o canal R
-            #show_histograma_groups(image)
+            #show_histograma(image[:, :, :1],False)  # RGBA, apenas mostra o canal R
+            show_histograma_groups(image[:,:,:1],False,True)
 
             # Sound
     if ".wav" in file:
-        sr, sound = wavfile.read("saxriff.wav")  # returns Sample Rate and Data
+        sr, sound = wavfile.read("D:\\Universidade\\Ano2\\TI\\TP1\\TI---FCTUC\\TP1\\data\\saxriff.wav")  # returns Sample Rate and Data
         sound = np.asarray(sound)
         show_histograma(sound[:, :1],False) # Apenas mostra o canal esquerdo do som
+        show_histograma_groups(sound[:,:1],False,False)
 
-def alfabeto(data):
-    def alfabeto(data):
-    new_data = []
-    for i in range(len(data)):
-        data[i]=str.lower(data[i])
-    group=2
-    y,counta = np.unique(data,return_counts=True)
+def get_txt_data(data,group):
+    new_data=[]
     i=0
-    while ( i < len(data)):
-        count=0
-        aux=""
-        j=i
-        flag=False
-        while(count<group):
-            if ( 'a'<=data[j]<='z' ) or (  'A'<=data[j]<='Z') or (  '0' <= data[j]<='9'):
-                aux+=str(data[j])
-                count+=1
-            j+=1
-            if (j==len(data)):
-                flag=True
+    while (i < len(data)):
+        count = 0
+        j = i
+        aux = ""
+        flag = False
+        while (count < group):
+            if ('a' <= data[j] <= 'z') or ('A' <= data[j] <= 'Z') or ('0' <= data[j] <= '9'):
+                aux += str(data[j])
+                count += 1
+            j += 1
+            if (j == len(data)):
+                flag = True
                 break
         if (flag):
             break
-        i=j
+        i = j
         new_data.append(aux)
-    new_data=np.asarray(new_data,dtype=str)
-    for i in new_data:
-        print(i)
-    x,values=np.unique(new_data, axis=0,return_counts=True)
+    return np.asarray(new_data, dtype=str)
+
+def get_bmp_data(data,group):
+    new_data=[]
+    if (data.ndim==2):
+        for i in range(len(data)):
+            for j in range(0, len(data[i]), group):
+                aux = ""
+                for k in range(j, j + group):
+                    aux += str(data[i, k])
+                new_data.append(aux)
+    else:
+        for i in range(len(data)):
+            for j in range(0, len(data[i]), group):
+                aux = ""
+                for k in range(j, j + group):
+                    aux += str(data[i, k, 0])
+                new_data.append(aux)
+    return np.asarray(new_data, dtype=str)
+
+def get_sound_data(data,group):
+    new_data=[]
+    for i in range(0,len(data),group):
+        aux = ""
+        for j in range(i, i + group):
+            aux += str(data[j, 0])
+            if (j+1==len(data)):
+                break
+        new_data.append(aux)
+    return np.asarray(new_data, dtype=str)
+
+#return simbolos agrupados//contagem deles
+def group_symbols(data, istxt,isbmp):
+    group=2
+    y,counta = np.unique(data, return_counts=True) if ~istxt else uniques(data)
+    if (istxt):
+        new_data=get_txt_data(data,group)
+    elif (isbmp):
+        new_data=get_bmp_data(data,group)
+    else:
+        new_data=get_sound_data(data,group)
+    x,values=np.unique(new_data,return_counts=True)
     print(x)
     print(entropy(counta,base=2))
     return x,values
