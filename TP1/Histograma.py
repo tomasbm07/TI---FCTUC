56272b4c7c607e4a46f61ac584a8e2134b0be2e2
joelpoliveira <58565967+joelpoliveira@users.noreply.github.com>
2020-10-26 16:16:59 +0000

    commit
    atualizar com main

--- TP1/Histograma.py
+++ TP1/Histograma.py
@@ -3,16 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import sounddevice as sd
 from scipy.io import wavfile
-from scipy.stats import entropy
-import math
 
 PATH = "data\\"
-
-#calc entropia com np.log2
-def entropia(valores):
-    total = np.sum(valores)
-    prob = valores[valores>0]/total
-    return np.sum(-np.log2(prob)*prob)
 
 
 # Represents a numpy array in a histogram
@@ -26,14 +18,8 @@ def histograma(x, values):
     plt.bar(x, values, align = "center")
     plt.show()
 
-#return simbolos agrupados//contagem deles
-def group_symbols(info):
-    group=2
-    new_info=[]
-    for i in range(0,int(np.prod(info.shape))-group,group):
-    	new_info.append(info[i:i+group])
-    new_info=np.asarray(new_info)
-    print(new_info)
-    return gerar_alfabeto(new_info)
-
-  #melhoramento de operação em todos os dados de array (exemplo: entropia) !entropia check! media check!
+#calc entropia com np.log2
+def entropia(valores):
+    total = np.sum(valores)
+    prob = valores[valores>0]/total
+    return np.sum(-np.log2(prob)*(prob))
