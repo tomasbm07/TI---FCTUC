5b0e686ce33656625b873d2f60dbd566309771d5
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-10-26 14:39:33 +0000

    Função gerar alfabeto

--- TP1/Histograma.py
+++ TP1/Histograma.py
@@ -3,52 +3,42 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import sounddevice as sd
 from scipy.io import wavfile
-from scipy.stats import entropy
 import math
 
 PATH = "data\\"
 
+
 #calc entropia com np.log2
-def np_entropia(valores):
+def entropia(valores):
     h=0
     total = np.sum(valores)
     for i in valores:
         h+=-np.log2(i/total)*(i/total)
     return h
 
-#contar apenas letras do alfabeto regulares e digitos
-def txt_uniques(data):
-    symbols = []
-    counts = []
-    for i in data:
-        if ((i not in symbols) and (('a' <= i <='z') or ('0'<= i <= '9') or ('A'<= i <= 'Z'))):
-            symbols.append(i)
-            counts.append(1)
-        elif i in symbols:
-            counts[symbols.index(i)]+=1
-    return np.asarray(symbols,dtype=str), np.asarray(counts,dtype=int)
 
 # Represents a numpy array in a histogram
-def show_histograma(data,istxt,isbmp):
-    #if not istxt:
-    #    x, values = np.unique(data, return_counts=True)
-    #else:
-    #    x,values = txt_uniques(data)
-    x,values=group_symbols(data,istxt,isbmp)
+def show_histograma(data, istxt, isbmp):
+    if istxt:
+    	x,values = txt_uniques(data)
+    else:
+    	x, values = np.unique(data, return_counts=True)
+        
+    #x,values=group_symbols(data, istxt, isbmp)
     plt.figure(0)
-    plt.annotate(f'H = {entropy((values),qk=None,base=2):.2f} bits/pixel', xy=(0, 0), xycoords=('axes fraction', 'figure fraction'),
+    plt.annotate(f'H = {entropia(values):.2f} bits/pixel', xy=(0, 0), xycoords=('axes fraction', 'figure fraction'),
                  xytext=(65, 5), textcoords='offset points', size=12, ha='right', va='bottom')
     plt.title("Histograma")
     plt.xlabel("Valores")
     plt.ylabel("Repetições")
-    plt.bar(x, values, align="center")
+    plt.bar(x, values, align = "center")
     plt.show()
 
 # Selects how to represent each file type
 def histograma(file):
     # Text
     if ".txt" in file:
-        file = open(PATH+file, "r")
+        file = open(PATH + file, "r")
         text = np.asarray(list(file.read()))
         show_histograma(text,True,False)
 
@@ -89,6 +79,19 @@ def get_txt_data(data,group):
         new_data.append(aux)
     return np.asarray(new_data, dtype=str)
 
+#contar apenas letras do alfabeto regulares e digitos
+def txt_uniques(data):
+    symbols = []
+    counts = []
+    for i in data:
+        if ((i not in symbols) and (('a' <= i <='z') or ('A'<= i <= 'Z'))):
+            symbols.append(i)
+            counts.append(1)
+        elif i in symbols:
+            counts[symbols.index(i)] += 1
+    return np.asarray(symbols, dtype=str), np.asarray(counts, dtype=int)
+    
+
 def get_bmp_data(data,group):
     new_data=[]
     if (data.ndim==2):
@@ -118,6 +121,7 @@ def get_sound_data(data,group):
         new_data.append(aux)
     return np.asarray(new_data, dtype=str)
 
+
 #return simbolos agrupados//contagem deles
 def group_symbols(data, istxt,isbmp):
     group=2
@@ -129,4 +133,4 @@ def group_symbols(data, istxt,isbmp):
         new_data=get_sound_data(data,group)
     x,values=np.unique(new_data,return_counts=True)
     print(x)
-    return x,values
+    return x, values
--- TP1/HuffmanCode.py
+++ TP1/HuffmanCode.py
@@ -5,7 +5,9 @@ import sounddevice as sd
 from scipy.io import wavfile
 from scipy.stats import entropy
 import huffmancodec as huff
+
 PATH = "data\\"
+
 
 def huffmanCode(data):
 	if '.bmp' in data:
@@ -17,17 +19,17 @@ def huffmanCode(data):
 			codec = huff.HuffmanCodec.from_data(image[:, :, :1].tobytes())
 			symbols2, weight=np.unique(image[:, :, :1], return_counts=True)
 
-	elif '.txt' in data:
+	if '.txt' in data:
 		file = open(PATH +data, "r")
 		text = np.asarray(list(file.read()))
 		codec = huff.HuffmanCodec.from_data(text)
 		symbols2, weight = np.unique(text, return_counts=True)
 		
-	elif ".wav" in data:
+	if ".wav" in data:
 		sr, sound = wavfile.read(PATH +data)
 		sound = np.asarray(sound)
 		codec = huff.HuffmanCodec.from_data(sound[:,:1].tobytes())
 		symbols2, weight = np.unique(sound[:, :1], return_counts=True)
 
 	symbols, length = codec.get_code_len()
-	print(np.average(length,weights=weight))
+	print(np.average(length, weights=weight))
