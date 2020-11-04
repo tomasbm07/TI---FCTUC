361e8513c7fe543ef46fa5a74ffa73cc99d1ec88
joelpoliveira <58565967+joelpoliveira@users.noreply.github.com>
2020-11-04 19:05:25 +0000

    commit

--- TP1/MutualInformation.py
+++ TP1/MutualInformation.py
@@ -7,14 +7,15 @@ from scipy.stats import entropy
 import huffmancodec as huff
 from Histograma import histograma
 from pyitlib import discrete_random_variable as drv
+import Funcoes as f
 
 PATH = "data\\"
 
 def plot_it(mutual_information):
 	plt.figure(1)
-	plt.bar(np.arange( len(mutual_information) ).tolist() , mutual_information.tolist())
+	plt.plot(np.arange(1, len(mutual_information)+1 ), mutual_information, 'o')
 	plt.ylabel("Informacao mutua")
-	plt.xlabel("Target_Test_n")
+	plt.xlabel("Song*x")
 	plt.annotate(f'Max: {mutual_information.max():.02f}', xy=(0, 0), xycoords=('axes fraction', 'figure fraction'),
                  xytext=(65, 5), textcoords='offset points', size=12, ha='right', va='bottom')
 	plt.show()
@@ -46,8 +47,16 @@ def shazam(querry, target, alfabeto, step):
 		mutual_info[index]=mutual_information(tabela)
 		index += 1
 		tabela = np.zeros((len(alfabeto), len(alfabeto)), dtype=float)
-	plot_it(mutual_info)
 	#print(mutual_info, len(mutual_info))
+	return mutual_info
+
+def compare_MIs(songs):
+	mt_infos=np.empty(0, dtype=float)
+	x, values, info = f.gerar_alfabeto("saxriff.wav")
+	for i in songs:
+		x2, values2, info2 = f.gerar_alfabeto(i)
+		mt_infos=np.append(mt_infos, shazam(info, info2, x, int( 0.25*len(info) ) ).max() )
+	plot_it(mt_infos)
 
 querry = np.asarray([2, 6 ,4 ,10 ,5, 9, 5 ,8, 0, 8], dtype=int)
 target = np.asarray([6,8,9,7,2,4,9,9,4,9,1,4,8,0,1,2,2,6,3,2,0,7,4,9,5,4,8,5,2,7,8,0,7,4,8,5,7,4,3,2,2,7,3,5,2,7,4,9,9,6], dtype=int)
--- TP1/main.py
+++ TP1/main.py
@@ -10,7 +10,7 @@ def main():
 	#lena.bmp, CT1.bmp, binaria.bmp, saxriff.wav, texto.txt
 	x, values, info = f.gerar_alfabeto("texto.txt")
 	"""Exercicios 1, 2 e 3"""
-	histograma(x, values)
+	#histograma(x, values)
 
 	"""Exercicio 4"""
 	
@@ -18,12 +18,13 @@ def main():
 	"""Exercicio 5"""
 	#x, values , info = f.group_symb(info)
 
-	huff.huffmanCode(info, values)
+	#huff.huffmanCode(info, values)
 
 	"""Exercicio 6"""
+	songs = ("Song01.wav", "Song02.wav", "Song03.wav", "Song04.wav", "Song05.wav", "Song06.wav", "Song07.wav")
 	#x2, values2, info2 = f.gerar_alfabeto("target01 - repeat.wav")
 	#mt.shazam(info, info2, x, int( 0.25*len(info) ) )
-	
+	mt.compare_MIs(songs)
 
 if __name__ == "__main__": 
     main()
