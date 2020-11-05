ff7909af818c3f0585f2042b9d73baf0c1fe322d
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-11-05 16:45:52 +0000

    change idk name

--- TP1/main.py
+++ TP1/main.py
@@ -24,15 +24,15 @@ def main():
 	#f.group_symb(info)
 
 	"""Exercicio 6"""
-	idk = []
+	im = []
 	x, values, info = f.gerar_alfabeto("saxriff.wav") #querry
 	#targets: "target01 - repeat.wav", "target02 - repeatNoise.wav"
 	target = "target01 - repeat.wav"
 	x2, values2, info2 = f.gerar_alfabeto(target) #target
-	idk = mt.shazam(info, info2, x, int((len(info)/4)))
+	im = mt.shazam(info, info2, x, int((len(info)/4)))
 
 	sr, data = wavfile.read("data\\"+target)
-	plt.plot(np.arange(0, int(len(info2)/sr), (len(info2)/len(idk))/sr), idk)
+	plt.plot(np.arange(0, int(len(info2)/sr), (len(info2)/len(im))/sr), im)
 	plt.title(f"Evolução Informação mútua em {target}")
 	plt.xlabel("Tempo(s)")
 	plt.ylabel("Informação Mútua")
@@ -43,10 +43,10 @@ def main():
 	#for i in range(1,8,1):	
 	#	print(f"Song0{i}:")
 	#	x2, values2, info2 = f.gerar_alfabeto(f"Song0{i}.wav")
-	#	idk = mt.shazam(info, info2, x, int(0.25*(len(info))))
-	#	plt.plot(np.arange(0, len(idk), 1), idk, label = f"Song0{i}", marker = 'o')
-		#ax.plot(np.arange(0, float(f"0.{len(idk)}"), 0.1), idk, label = f"Song0{i}", marker = 'o')
-		#ax2.plot(np.arange(0, float(f"0.{len(idk)}"), 0.1), idk, label = f"Song0{i}", marker = 'o')
+	#	im = mt.shazam(info, info2, x, int(0.25*(len(info))))
+	#	plt.plot(np.arange(0, len(im), 1), im, label = f"Song0{i}", marker = 'o')
+		#ax.plot(np.arange(0, float(f"0.{len(im)}"), 0.1), im, label = f"Song0{i}", marker = 'o')
+		#ax2.plot(np.arange(0, float(f"0.{len(im)}"), 0.1), im, label = f"Song0{i}", marker = 'o')
 
 	"""ax2.set_ylim(0.08,0.24)
 	ax.set_ylim(0.4, 4)
