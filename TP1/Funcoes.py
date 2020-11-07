50363d7592abd178d6d56ef1a44f43e3e0aac3c1
joelpoliveira <58565967+joelpoliveira@users.noreply.github.com>
2020-11-07 15:31:29 +0000

    commit main
    retirar módulos não usados de ficheiros

--- TP1/Funcoes.py
+++ TP1/Funcoes.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as img
-import sounddevice as sd
 from scipy.io import wavfile
-from scipy.stats import entropy
 import huffmancodec as huff
 from Histograma import histograma
 
